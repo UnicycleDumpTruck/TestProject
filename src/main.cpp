8a0dd5b6df042e35ef471bc7ac959809520cb234
UnicycleDumpTruck <junk@jeffhighsmith.com>
2021-11-13 13:36:00 -0500

    Established basic blink functions, added pinout image

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define LED_PIN 13
+
+void ledSetup();
+void ledOn();
+void ledOff();
+void wait();
+
+void setup()
+{
+  ledSetup();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  ledOn();
+  wait();
+  ledOff();
+}
+
+void ledSetup()
+{
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void ledOn()
+{
+  digitalWrite(LED_PIN, HIGH);
+}
+
+void ledOff()
+{
+  digitalWrite(LED_PIN, LOW);
+}
+
+void wait()
+{
+  delay(500);
 }
